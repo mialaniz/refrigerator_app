ebbf5e8277b94b5a7b433c22c2a449cb5b3cc23b
Mialaniz <michaelalaniz76@gmail.com>
2024-10-05 18:46:19 -0700

    first commit

---
+++ .next/static/webpack/app/page.3bd0ca7f2bc257a5.hot-update.js
@@ -0,0 +1,21 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("app/page",{
+
+/***/ "(app-pages-browser)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%7B%22request%22%3A%22C%3A%5C%5CUsers%5C%5Cmicha%5C%5CDocuments%5C%5Crefrigerator_app%5C%5Cnode_modules%5C%5C%40mui%5C%5Cmaterial%5C%5CPaper%5C%5CPaper.js%22%2C%22ids%22%3A%5B%22default%22%5D%7D&server=false!":
+/*!******************************************************************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%7B%22request%22%3A%22C%3A%5C%5CUsers%5C%5Cmicha%5C%5CDocuments%5C%5Crefrigerator_app%5C%5Cnode_modules%5C%5C%40mui%5C%5Cmaterial%5C%5CPaper%5C%5CPaper.js%22%2C%22ids%22%3A%5B%22default%22%5D%7D&server=false! ***!
+  \******************************************************************************************************************************************************************************************************************************************************************************************************************/
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+eval(__webpack_require__.ts("Promise.resolve(/*! import() eager */).then(__webpack_require__.bind(__webpack_require__, /*! ./node_modules/@mui/material/Paper/Paper.js */ \"(app-pages-browser)/./node_modules/@mui/material/Paper/Paper.js\"));\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1wYWdlcy1icm93c2VyKS8uL25vZGVfbW9kdWxlcy9uZXh0L2Rpc3QvYnVpbGQvd2VicGFjay9sb2FkZXJzL25leHQtZmxpZ2h0LWNsaWVudC1lbnRyeS1sb2FkZXIuanM/bW9kdWxlcz0lN0IlMjJyZXF1ZXN0JTIyJTNBJTIyQyUzQSU1QyU1Q1VzZXJzJTVDJTVDbWljaGElNUMlNUNEb2N1bWVudHMlNUMlNUNyZWZyaWdlcmF0b3JfYXBwJTVDJTVDbm9kZV9tb2R1bGVzJTVDJTVDJTQwbXVpJTVDJTVDbWF0ZXJpYWwlNUMlNUNQYXBlciU1QyU1Q1BhcGVyLmpzJTIyJTJDJTIyaWRzJTIyJTNBJTVCJTIyZGVmYXVsdCUyMiU1RCU3RCZzZXJ2ZXI9ZmFsc2UhIiwibWFwcGluZ3MiOiJBQUFBLGdOQUE4SiIsInNvdXJjZXMiOlsid2VicGFjazovL19OX0UvPzU1ZDMiXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0KC8qIHdlYnBhY2tNb2RlOiBcImVhZ2VyXCIsIHdlYnBhY2tFeHBvcnRzOiBbXCJkZWZhdWx0XCJdICovIFwiQzpcXFxcVXNlcnNcXFxcbWljaGFcXFxcRG9jdW1lbnRzXFxcXHJlZnJpZ2VyYXRvcl9hcHBcXFxcbm9kZV9tb2R1bGVzXFxcXEBtdWlcXFxcbWF0ZXJpYWxcXFxcUGFwZXJcXFxcUGFwZXIuanNcIik7XG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///(app-pages-browser)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%7B%22request%22%3A%22C%3A%5C%5CUsers%5C%5Cmicha%5C%5CDocuments%5C%5Crefrigerator_app%5C%5Cnode_modules%5C%5C%40mui%5C%5Cmaterial%5C%5CPaper%5C%5CPaper.js%22%2C%22ids%22%3A%5B%22default%22%5D%7D&server=false!\n"));
+
+/***/ })
+
+});
---
+++ components/Button.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Button = () => {
+  return (
+    <div className="flex justify-center">
+        <form>
+            <button className="bg-red-800 w-32 rounded-lg">
+                Submit Photo
+            </button>
+        </form>
+    </div>
+  )
+}
