810f0abcfd4e3236f9b74358ec5bc4ef5792296a
Mialaniz <michaelalaniz76@gmail.com>
2024-10-07 23:06:45 -0700

    working backend

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,30 @@
 const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser')
+const http = require('http');
+const predictRouter = require('./routes/predict');
+
 const app = express();
-const port = 3000;
+// We added bodyParser so that we can access `body` in `req` later
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/hello', function (req, res) { 
+  res.send("hello")
 });
+app.use('/predict', predictRouter);
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.get('/', function (req, res) {
+  return res.sendFile(path.join(__dirname, "build", "index.html"))
+})
+
+const port = process.env.PORT || '8080';
+
+app.set('port', port);
+const server = http.createServer(app);
+server.listen(port);
+
+server.on('listening', () => {
+  console.log('Listening on ' + (port));
 });
--- backend/routes/predict.js
+++ backend/routes/predict.js
@@ -1,9 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+const dotenv = require('dotenv');
+dotenv.config();
+
 const {ClarifaiStub, grpc} = require("clarifai-nodejs-grpc");
 
 const stub = ClarifaiStub.grpc();
 
 const metadata = new grpc.Metadata();
-metadata.set("authorization", "Key ${process.env.CLARIFI_KEY}");
+metadata.set("authorization", `Key ${process.env.CLARIFI_KEY}`);
 
 function prediction(imageURL) {
 
@@ -12,7 +18,7 @@ function prediction(imageURL) {
             {
                 // This is the model ID of a publicly available General model. You may use any other public or custom model ID.
                 model_id: "aaa03c23b3724a16a56b629203edc62c",
-                inputs: [{data: {image: {url: imageURL}}}]
+                inputs: imageURL
             },
             metadata,
             (err, response) => {
@@ -25,13 +31,35 @@ function prediction(imageURL) {
                     console.log("Received failed status: " + response.status.description + "\n" + response.status.details);
                     return;
                 }
-        
+                
+                let results = [];
                 console.log("Predicted concepts, with confidence values:")
+                
+                // grabs the outputs of the data such as the name and the value.
                 for (const c of response.outputs[0].data.concepts) {
-                    console.log(c.name + ": " + c.value);
+                    results.push({ // makes the array with the values and names.
+                        name: c.name,
+                        value: c.value
+                    })
                 }
             }
         );
     })
 
 }
+
+router.post('/', async (req,res, next) => {
+    try{
+        const {imageURL} = req.body;
+        const inputs = [{data:{image:{url: imageURL}}}];
+
+        const results = await prediction(inputs);
+
+        return res.send(results);
+    } catch(err) {
+        return res.status(400).send(err);
+
+    }
+})
+
+module.exports = router;
